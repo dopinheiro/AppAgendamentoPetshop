18fa0bd0c27ff6f9534a16e73bc04ff4e25a4224
Milena de Oliveira <milemoliveira@gmail.com>
2021-10-21 13:47:49 -0300

    Deletados arquivos não utilizados

--- lib/pages/app_widget.dart
+++ lib/pages/app_widget.dart
@@ -4,7 +4,7 @@ import 'package:petshop/pages/homePage.dart';
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'PetShop', 
       home: HomePage());
   }
--- lib/pages/splashPage.dart
+++ lib/pages/splashPage.dart
@@ -1,22 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:petshop/pages/homePage.dart';
 import 'package:petshop/utils/colors/appGradients.dart';
 class SplashPage extends StatelessWidget {
   const SplashPage({Key key}) : super(key: key);
-
-  BuildContext get context => null;
-
-  void handleTimeout() {
-  Navigator.of(context).pushReplacement(new MaterialPageRoute(
-    builder: (BuildContext context) => new HomePage()));
-}
-
-startTimeout() async {
-  var duration = const Duration(seconds: 3);
-  return new Timer(duration, handleTimeout);
-}
 
   @override
   Widget build(BuildContext context) {
