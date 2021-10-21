b935e029ba3932c403968d07191c6cc7e5c664ed
Milena de Oliveira <67964254+milemoliveira@users.noreply.github.com>
2021-10-21 19:04:42 -0300

    Atualizado menu e splash (#2)

--- lib/utils/widgets/bottomAppBar.dart
+++ lib/utils/widgets/bottomAppBar.dart
@@ -2,36 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:petshop/utils/colors/appColors.dart';
 
-class BottomAppbar extends StatelessWidget {
+class BottomAppbar extends StatefulWidget {
+
+  @override
+  _BottomAppbarState createState() => _BottomAppbarState();
+}
+
+class _BottomAppbarState extends State<BottomAppbar> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: AppColors.gray,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(icon: 
-                FaIcon(FontAwesomeIcons.home, size: 30, color: AppColors.brown), 
-                onPressed: () {  }),
-              IconButton(icon: 
-                FaIcon(FontAwesomeIcons.paw, size: 30, color: AppColors.brown), 
-                onPressed: () {  }),
-              IconButton(icon: 
-                FaIcon(FontAwesomeIcons.calendar, size: 30, color: AppColors.brown), 
-                onPressed: () {  }),
-              IconButton(icon: 
-                FaIcon(FontAwesomeIcons.briefcase, size: 30, color: AppColors.brown), 
-                onPressed: () {  }),
-              IconButton(icon: 
-                FaIcon(FontAwesomeIcons.cog, size: 30, color: AppColors.brown), 
-                onPressed: () {  }),
-            ],
-          ),
-        ),
-      )
-    ); 
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.gray,
+        selectedItemColor: AppColors.yellow,
+        unselectedItemColor: AppColors.brown,
+        unselectedFontSize: 12,
+        selectedFontSize: 15,
+        currentIndex: _currentIndex,
+        
+        onTap: (value) {
+          // Respond to item press.
+          setState(() => _currentIndex = value);
+        },
+
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.home, size: 30),
+            label: "Início", ),
+
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.paw, size: 30),
+            label: "Pets"),
+
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.calendar, size: 30),
+            label: "Agenda"),
+          
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.briefcase, size: 30),
+            label: "Serviços"),
+          
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.cog, size: 30),
+            label: "Config"),
+        ],),
+    );
   }
 }
+       
+
+
+       
