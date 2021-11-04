2c0fe485aff138f54d528c0f041f9b179ae68b5f
Milena de Oliveira <67964254+milemoliveira@users.noreply.github.com>
2021-11-04 19:40:50 -0300

    Agenda (#6)
    * Inicio da tela de servicos
    
    * Atualizada página de serviços
    
    * Criada config page
    
    * Página de agendamentos

--- lib/pages/appointmentsPage.dart
+++ lib/pages/appointmentsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/utils/colors/appColors.dart';
 
 class AppointmentsPage extends StatefulWidget {
   @override
@@ -8,6 +9,198 @@ class AppointmentsPage extends StatefulWidget {
 class _AppointmentsPageState extends State<AppointmentsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Center(child: 
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Text("Agendamentos",
+            style: TextStyle(
+              fontSize: 20,
+              color: AppColors.brown,
+              fontWeight: FontWeight.bold
+            ),),
+          ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height*0.15,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: AppColors.ligthYellow,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Container(
+                child:
+                Row(
+                children: [
+                Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.calendar_today, color: AppColors.brown),
+                Icon(Icons.timer, color: AppColors.brown)
+              ],),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                    Text("  04/11/2021", 
+                    style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),),
+                    Text("  15:00",
+                     style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),)
+              ],),
+                ),
+                ],)),
+              
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.delete_forever, color: AppColors.brown),
+                Icon(Icons.edit, color: AppColors.brown)
+              ],),
+                ]
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height*0.15,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: AppColors.ligthYellow,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Container(
+                child:
+                Row(
+                children: [
+                Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.calendar_today, color: AppColors.brown),
+                Icon(Icons.timer, color: AppColors.brown)
+              ],),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                    Text("  20/11/2021", 
+                    style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),),
+                    Text("  15:00",
+                     style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),)
+              ],),
+                ),
+                ],)),
+              
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.delete_forever, color: AppColors.brown),
+                Icon(Icons.edit, color: AppColors.brown)
+              ],),
+                ]
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height*0.15,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: AppColors.ligthYellow,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Container(
+                child:
+                Row(
+                children: [
+                Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.calendar_today, color: AppColors.brown),
+                Icon(Icons.timer, color: AppColors.brown)
+              ],),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                    Text("  04/12/2021", 
+                    style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),),
+                    Text("  15:00",
+                     style: TextStyle(
+                      color: AppColors.brown,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),)
+              ],),
+                ),
+                ],)),
+              
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Icon(Icons.delete_forever, color: AppColors.brown),
+                Icon(Icons.edit, color: AppColors.brown)
+              ],),
+                ]
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(AppColors.purple),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/newappointments');
+            },
+            child: const Text("Novo Agendamento", 
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 15,
+            ),),
+      ),
+],
+        ),
+      )
+    );
   }
 }
--- lib/pages/servicesPage.dart
+++ lib/pages/servicesPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/utils/colors/appColors.dart';
 
 class ServicesPage extends StatefulWidget {
   @override
@@ -7,7 +8,151 @@ class ServicesPage extends StatefulWidget {
 
 class _ServicesPageState extends State<ServicesPage> {
   @override
+
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            GridView.count(
+              padding: EdgeInsets.fromLTRB(40, 40, 40, 0),
+              shrinkWrap: true,
+              primary: true,
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              // childAspectRatio: MediaQuery.of(context).size.height * 50,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/chuveiro.png', width: 60),
+                    Text("Banho", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,)
+                    ),
+                  ],),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/tesoura.png', width: 60,),
+                    Text("Tosa", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,)
+                    )
+                  ],),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/shampoo.png', width: 60,),
+                    Text("Hidratação", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,)
+                    )
+                  ],),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/escova.png', width: 60,),
+                    Text("Escovação", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,
+                      )
+                    )
+                  ],),
+                 decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/pente.png', width: 60,),
+                    Text("Penteados", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,)
+                    )
+                  ],),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    Image.asset('assets/images/pata.png', width: 60,),
+                    Text("Corte de unhas", 
+                    style: TextStyle(
+                      color: AppColors.brown, 
+                      fontSize: 15, 
+                      fontWeight: FontWeight.bold,)
+                    )
+                  ],),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.yellow,),
+                ),
+
+              ],
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(AppColors.purple),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/navigation');
+                 },
+                child: Text("Novo Agendamento", 
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+                ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
